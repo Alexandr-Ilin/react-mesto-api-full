0e4c4ae729befd1898424d52b469b93fbcc14358
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-08-24 19:25:08 +0300

    CurUS-nook

--- frontend/src/components/Popup.js
+++ frontend/src/components/Popup.js
@@ -1,30 +1,28 @@
-import React from "react";
+import React from 'react';
 
-    // создаем отдельный компонент `Popup` для обертки любых попапов
-const Popup = ({ isOpen, name, onClose, children, container }) => {
-
-    React.useEffect(() => {
+// создаем отдельный компонент `Popup` для обертки любых попапов
+const Popup = ({
+  isOpen, name, onClose, children, container,
+}) => {
+  React.useEffect(() => {
     if (!isOpen) return;
 
     const closeByEscape = (e) => {
       if (e.key === 'Escape') {
         onClose();
       }
+    };
+    document.addEventListener('keydown', closeByEscape);
+    return () => document.removeEventListener('keydown', closeByEscape);
+  }, [isOpen, onClose]);
+  const handleOverlay = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
     }
-    
-    document.addEventListener('keydown', closeByEscape)
-    return () => document.removeEventListener('keydown', closeByEscape)
-  }, [isOpen, onClose])
-    
-    const handleOverlay = (e) => {
-      if (e.target === e.currentTarget) {
-          onClose();
-      }
-    }
-    
-    return (
+  };
+  return (
       <section
-        className={`popup ${isOpen ? "popup_opened" : ""} popup_type_${name}`}
+        className={`popup ${isOpen ? 'popup_opened' : ''} popup_type_${name}`}
         onClick={handleOverlay}
       >
         <div className={`popup__${container}`}>
@@ -36,6 +34,6 @@ const Popup = ({ isOpen, name, onClose, children, container }) => {
           />
         </div>
       </section>
-    );
-}
+  );
+};
 export default Popup;
--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect } from 'react-router-dom';
 
-const ProtectedRoute = ({ loggedIn, children, ...props }) => {
+const ProtectedRoute = ({
+  loggedIn, children, ...props
+}) => {
   return (
     <Route {...props}>
       { loggedIn ? children : <Redirect to="/sign-in" />}
     </Route>
-  )
-}
+  );
+};
 
 export default ProtectedRoute;
